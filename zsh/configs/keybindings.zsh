95ca0e18c998feeb97efb8b8e2f3d125d6152594
Michael Anhari <mpanhari@gmail.com>
2019-06-02 11:34:30 -0400

    Clean up zsh keybindings

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -1,6 +1,10 @@
 # give us access to ^Q
 stty -ixon
 
+# Use cat or Ctrl-V to find new bindings
+# https://stackoverflow.com/questions/5314161/interpret-zsh-bindkey-escaped-sequences
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
+
 # vi mode
 bindkey -v
 bindkey "^F" vi-cmd-mode
@@ -15,3 +19,11 @@ bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+# Remap Ctrl-G to Alt-C for using FZF for cd
+# https://github.com/junegunn/fzf#installation
+bindkey -s "^G" "^[c"
+# Map Ctrl-O to jump back to the last directory
+bindkey -s "^O" "cd -\n clear\n"
+# Map Ctrl-Spacebar to clear the screen (tmux breaks cmd+k)
+bindkey -s "^@" "clear\n"

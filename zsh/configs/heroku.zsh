b6f544d8f854e38bf38a0626a68fda12c2f08661
Michael Anhari <mpanhari@gmail.com>
2019-06-07 18:27:25 -0400

    Add link to heroku autocomplete instructions

--- zsh/configs/heroku.zsh
+++ zsh/configs/heroku.zsh
@@ -1,2 +1,3 @@
 # heroku autocomplete setup
+# https://devcenter.heroku.com/articles/heroku-cli-autocomplete#command-name-completion
 HEROKU_AC_ZSH_SETUP_PATH=/Users/michaelanhari/Library/Caches/heroku/autocomplete/zsh_setup && test -f $HEROKU_AC_ZSH_SETUP_PATH && source $HEROKU_AC_ZSH_SETUP_PATH;

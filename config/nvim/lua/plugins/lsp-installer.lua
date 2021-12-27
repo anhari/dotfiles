63641828cbe0f8f1ba0d96b9ddb8db5bfc222039
Michael Anhari <mpanhari@gmail.com>
2021-12-27 18:56:32 -0500

    Wire up go-to def and preview

--- config/nvim/lua/plugins/lsp-installer.lua
+++ config/nvim/lua/plugins/lsp-installer.lua
@@ -1,10 +1,18 @@
 local lsp_installer = require("nvim-lsp-installer")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
 
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances
 -- instead (see example below).
 lsp_installer.on_server_ready(function(server)
     local opts = {}
+
+    opts.capabilities = cmp_nvim_lsp.update_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
 
     -- (optional) Customize the options passed to the server
     if server.name == "sumneko_lua" then
--- config/nvim/lua/plugins/saga.lua
+++ config/nvim/lua/plugins/saga.lua
@@ -1,7 +1,15 @@
 require('lspsaga').init_lsp_saga()
 
 -- find the cursor word definition and reference
-vim.api.nvim_set_keymap("n", "L", ":Lspsaga lsp_finder<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "L", ":Lspsaga hover_doc<cr>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>ca", ":Lspsaga code_action<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<leader>ca", ":<C-U>Lspsaga range_code_action<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "]r", ":Lspsaga diagnostic_jump_next<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "[r", ":Lspsaga diagnostic_jump_previous<cr>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<C-space>", ":Lspsaga code_action<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-space>", ":<C-U>Lspsaga range_code_action<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<S-space>", ":Lspsaga rename<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gp", ":Lspsaga preview_definition<cr>", { noremap = true, silent = true })

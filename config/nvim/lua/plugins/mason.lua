02b5e64b178411be62953ab60ce0bfa190e99a5e
Michael Anhari <michaelanhari@icloud.com>
2024-05-06 11:20:28 -0400

    Set kitty theme to moon for contrast

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -32,7 +32,11 @@ return {
 				"vimls",
 			},
 		})
-
+		-- require("mason-lspconfig").setup_handlers({
+		-- 	function(server_name)
+		-- 		require("lspconfig")[server_name].setup({})
+		-- 	end,
+		-- })
 		require("lspconfig").bashls.setup({})
 		require("lspconfig").cssls.setup({
 			settings = {
